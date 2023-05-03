0571a83557ad501b45541a80681e6197ae0bd55c
AFarahnoz <132000808+AFarahnoz@users.noreply.github.com>
2023-05-03 20:20:41 +0500

    Update Student.php

--- app/Models/Student.php
+++ app/Models/Student.php
@@ -8,4 +8,34 @@ use Illuminate\Database\Eloquent\Model;
 class Student extends Model
 {
     use HasFactory;
+
+    public function academicDebts()
+    {
+    	return $this->hasMany(AcademicDebt::class);
+    }
+
+    public function attendance() 
+    {
+    	return $this->hasMany(Attendance::class);
+    }
+
+    public function group() 
+    {
+    	return $this->hasOne(Group::class);
+    }
+
+    public function progress()
+    {
+    	return $this->hasMany(Progress::class);
+    }
+
+    public function parents()
+    {
+    	return $this->hasMany(Parent::class);
+    }
+
+    public function achievements()
+    {
+    	return $this->hasMany(Achievement::class);
+    }
 }
